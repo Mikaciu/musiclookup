dc08e26d6cbab97fcb4c58b5896aa1f81dfa9080
Mikaël Hautin <mikael.hautin@gmail.com>
2016-11-15 17:43:38 +0100

    Create musiclookup.py

---
+++ musiclookup.py
@@ -0,0 +1,31 @@
+import musicbrainzngs
+
+musicbrainzngs.set_useragent("musiclookup", "0.0", contact=None)
+# musicbrainzngs.set_hostname("musicbrainz-mirror.eu:5000")
+
+def get_all_releases_from_artist(s_artist_id):
+    limit = 100
+    offset = 0
+    page = 1
+    d_search_parameters = dict(artist=s_artist_id, release_type=["album"])
+
+    first_page = musicbrainzngs.browse_releases(limit=None, **d_search_parameters)
+    page_releases = first_page['release-list']
+
+    yield from ({"title": albums["title"], "date": albums["date"]} for albums in page_releases)
+
+    while len(page_releases) >= limit:
+        offset += limit
+        page += 1
+        print("fetching page number %d.." % page)
+        next_page = musicbrainzngs.browse_releases(limit=limit, offset=offset, **d_search_parameters)
+        page_releases = next_page['release-list']
+        yield from ({"title": albums["title"], "date": albums["date"]} for albums in page_releases)
+
+
+result = musicbrainzngs.search_artists(artist="aerosmith", type="group", strict=True)
+for artist in result['artist-list']:
+    print("{id} {name}".format(**artist))
+
+    for album in get_all_releases_from_artist(artist["id"]):
+        print(album)
